71c8542b3f16a68542892e5d166ddb8c4f64959b
Jacinto Wong <contact@jacinto.design>
2019-05-22 17:56:30 -0400

    refinements on mobile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 declare var $: any;
 declare var require: any;
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,7 +14,26 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  //   $('#carouselExample').on('slide.bs.carousel', function (e) {
 
+  //     var $e = $(e.relatedTarget);
+  //     var idx = $e.index();
+  //     var itemsPerSlide = 1;
+  //     var totalItems = $('.carousel-item').length;
+      
+  //     if (idx >= totalItems-(itemsPerSlide-1)) {
+  //         var it = itemsPerSlide - (totalItems - idx);
+  //         for (var i=0; i<it; i++) {
+  //             // append slides to end
+  //             if (e.direction=="left") {
+  //                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+  //             }
+  //             else {
+  //                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+  //             }
+  //         }
+  //     }
+  // });
   }
 
   
