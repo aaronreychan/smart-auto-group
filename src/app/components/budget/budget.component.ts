5b127353b1cfc547ed7c686da00077d9fd930c53
Jacinto Wong <contact@jacinto.design>
2019-06-01 21:34:10 -0400

    finished select/budget page

--- src/app/components/budget/budget.component.ts
+++ src/app/components/budget/budget.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -10,6 +10,20 @@ export class BudgetComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(".js-range-slider").ionRangeSlider({
+      type: "single",
+      prefix: "$",
+      skin: "round",
+      min: 200,
+      max: 900,
+      from: 500,
+      step: 25,
+      grid: true,
+      onUpdate: function (data) {
+        // fired on changing slider with Update method
+        console.log(data);
+    }
+    });
   }
 
 }
