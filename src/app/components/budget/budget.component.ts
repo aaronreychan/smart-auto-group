6faf171070b94fa2193773d469c6d78bbc7a8db3
JACINTO WONG <jacinto.wong@cbc.ca>
2019-06-11 13:35:22 -0400

    added cookie functionality

--- src/app/components/budget/budget.component.ts
+++ src/app/components/budget/budget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Criteria } from '../../entities/criteria';
 import { MyCookieService } from '../../services/cookieService';
 import { AppConstants } from 'src/app/constants/AppConstants';
@@ -10,15 +10,17 @@ declare var $: any;
   templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.css']
 })
-export class BudgetComponent implements OnInit {
+export class BudgetComponent implements OnInit, AfterViewInit {
 
   criteria: Criteria;
 
   constructor(private myCookieService: MyCookieService) { }
 
   ngOnInit() {
-    this.myCookieService.storeCookie(AppConstants.cookieBudget, '200');
-    //this.criteria.budget = this.myCookieService.getCookie(AppConstants.cookieBudget);
+    this.myCookieService.storeCookie(AppConstants.cookieBudget, '250');
+    this.myCookieService.storeCookie(AppConstants.cookieCarType, 'car');
+    this.criteria = new Criteria();
+    this.criteria.budget = this.myCookieService.getCookie(AppConstants.cookieBudget);
 
     $(".js-range-slider").ionRangeSlider({
       type: "single",
@@ -26,14 +28,26 @@ export class BudgetComponent implements OnInit {
       skin: "round",
       min: 200,
       max: 900,
-      from: 500,
+      from: this.criteria.budget,
       step: 25,
       grid: true,
-      onUpdate: function (data) {
-        // fired on changing slider with Update method
-        console.log(data);
-    }
+      onFinish: function (data) {
+          // fired on changing slider with Update method
+          console.log(data);
+          debugger;
+          document.cookie = AppConstants.cookieBudget + '=' + data.from + ';';
+      }
     });
+
+  }
+
+  ngAfterViewInit(){
+    var carType = this.myCookieService.getCookie(AppConstants.cookieCarType);
+    $("#"+carType).prop('checked', true);
+  }
+  
+  public storeCookie(name:any, val: any) {
+    this.myCookieService.storeCookie(name, val);
   }
 
 }
