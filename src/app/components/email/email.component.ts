187efc20ca21973e425dd942961f3d6648072b0b
Jacinto Wong <contact@jacinto.design>
2019-06-26 09:32:03 -0400

    removed email functionality attempt

--- src/app/components/email/email.component.ts
+++ src/app/components/email/email.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Headers, Response, URLSearchParams } from '@angular/http';
 
 @Component({
   selector: 'app-email',
@@ -8,31 +7,9 @@ import { Http, Headers, Response, URLSearchParams } from '@angular/http';
 })
 export class EmailComponent implements OnInit {
 
-  constructor(private http: Http) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  sendEmail() {
-
-    let url = `https://us-central1-cloud-functions-jacinto.cloudfunctions.net/httpEmail`
-    let params: URLSearchParams = new URLSearchParams();
-    let headers = new Headers({'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-
-    params.set('to', 'kingofpandora@gmail.com');
-    params.set('from', 'jacintonwong@gmail.com');
-    params.set('subject', 'test-email');
-    params.set('content', 'Hello World');
-
-    return this.http.post(url, params)
-                    .toPromise()
-                    .then( res => {
-                      console.log(res)
-                    })
-                    .catch(err => {
-                      console.log(err)
-                    })
-
   }
 
 }
