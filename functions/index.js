8e930730ea9761416091676c7e04253b1a2032cf
Irina Grevtsova <igrevtsova@gmail.com>
2019-08-03 13:58:58 +0300

    anonymous photo added

--- functions/index.js
+++ functions/index.js
@@ -1,49 +1,50 @@
 const functions = require('firebase-functions');
 const helper = require('sendgrid').mail;
-const sg = require('sendgrid')('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'); // Sendgrid key
+const sg = require('sendgrid')(functions.config().sendgrid.key); // Sendgrid key
+
 
 function parseBody(value) {
-  const body = JSON.parse(value);
-  const fromEmail = new helper.Email(body.from); // user email
-  const toEmail = new helper.Email('George@smartautogroup.ca'); // main email
-  const subject = 'Smart Auto Group'; // subject
-  const content = new helper.Content('text/html', body.content); // HTML content
-  const mail = new helper.Mail(fromEmail, subject, toEmail, content);
-  return mail.toJSON();
+    const body = JSON.parse(value);
+    const fromEmail = new helper.Email(body.from); // user email
+    const toEmail = new helper.Email(functions.config().sendgrid.email); // main email 'George@smartautogroup.ca'
+    const subject = 'Smart Auto Group'; // subject
+    const content = new helper.Content('text/html', body.content); // HTML content
+    const mail = new helper.Mail(fromEmail, subject, toEmail, content);
+    return mail.toJSON();
 }
 
 exports.httpEmail = functions.https.onRequest((req, res) => {
     return Promise.resolve()
-      .then(() => {
+        .then(() => {
         if (req.method !== 'POST') {
-          const error = new Error('Only POST requests are accepted');
-          error.code = 405;
-          throw error;
-        }
+    const error = new Error('Only POST requests are accepted');
+    error.code = 405;
+    throw error;
+}
 
-        const request = sg.emptyRequest({
-          method: 'POST',
-          path: '/v3/mail/send',
-          body: parseBody(req.body)
-        });
+const request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: parseBody(req.body)
+});
 
-        return sg.API(request, (error, response) => {
-          console.log(response.statusCode);
-          console.log(response.body);
-        });
-      })
-      .then((response) => {
-        if (response) {
-          res.send(response);
-          return 'Success'
-        } else {
-          res.end();
-          return 'Error'
-        }
-      })
+return sg.API(request, (error, response) => {
+    console.log(response.statusCode);
+console.log(response.body);
+});
+})
+.then((response) => {
+    if (response) {
+        res.send(response);
+        return 'Success'
+    } else {
+        res.end();
+return 'Error'
+}
+})
 
-      .catch((err) => {
-        console.error(err);
-        return Promise.reject(err);
-      });
+.catch((err) => {
+    console.error(err);
+return Promise.reject(err);
+});
 });
