5b9221e42741d0aff5233e5ae1a765d873c87396
Irina Grevtsova <igrevtsova@gmail.com>
2019-10-01 15:29:43 +0300

    UI fixes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,30 +9,8 @@ declare var require: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  //   $('#carouselExample').on('slide.bs.carousel', function (e) {
-
-  //     var $e = $(e.relatedTarget);
-  //     var idx = $e.index();
-  //     var itemsPerSlide = 1;
-  //     var totalItems = $('.carousel-item').length;
-
-  //     if (idx >= totalItems-(itemsPerSlide-1)) {
-  //         var it = itemsPerSlide - (totalItems - idx);
-  //         for (var i=0; i<it; i++) {
-  //             // append slides to end
-  //             if (e.direction=="left") {
-  //                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
-  //             }
-  //             else {
-  //                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
-  //             }
-  //         }
-  //     }
-  // });
-  }
 }
